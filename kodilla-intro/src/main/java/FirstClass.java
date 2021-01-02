a2802f64dfcffc74f0affaf7cf709d953cb050af
Kamil Kr <krol-kamil@wp.pl>
2021-01-02 20:57:36 +0100

    deklarowanie i wywoływanie zmiennych

--- kodilla-intro/src/main/java/FirstClass.java
+++ kodilla-intro/src/main/java/FirstClass.java
@@ -1,5 +1,6 @@
 public class FirstClass {
     public static void main(String[] args) {
-        System.out.println('H');
+        String example = "Hello from FirstClass";
+        System.out.println(example);
     }
 }
---
+++ kodilla-intro/src/main/java/MainExtended.java
@@ -0,0 +1,15 @@
+public class MainExtended {
+    public static void main (String[] args){
+        String myName = "Kamil";
+        int yearOfBirth = 1987;
+        double periodOfEmployment = 5.2;
+        char carClass = 'A';
+        boolean isEmployed = true;
+        String jobTitle = "Doradca";
+        System.out.println(myName);
+        System.out.println(periodOfEmployment);
+        System.out.println(carClass);
+        System.out.println(isEmployed);
+        System.out.println(jobTitle);
+    }
+}
