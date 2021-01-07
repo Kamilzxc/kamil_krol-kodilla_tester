076d817a2581e7fda67a893fdaeb4666a046e118
Kamil Kr <krol-kamil@wp.pl>
2021-01-07 21:26:22 +0100

    prework

--- kodilla-intro/src/main/java/SimpleArray.java
+++ kodilla-intro/src/main/java/SimpleArray.java
@@ -1,2 +1,14 @@
-package PACKAGE_NAME;public class SimpleArray {
-}
+public class SimpleArray {
+    public static void main(String[] args) {
+        String[] names = new String[] {"Rafał", "Damian", "Piotr", "Grzegorz", "Dariusz"};
+
+        String name = names[2];
+        System.out.println(name);
+
+        int numberOfElements = names.length;
+        if (names.length == 5) {
+            System.out.println("moja tablica zawiera 5 elementów");
+        }
+
+        }
+    }
